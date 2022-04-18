bd2c16086e29e8b9e08a7b4257063b937090c4a7
jeel <koradiyajeel95@gmail.com>
2022-04-18 20:26:56 +0530

    task-4

--- Task-3/js/jquery.js
+++ Task-3/js/jquery.js
@@ -1,200 +1,198 @@
     $(document).ready(function () {
-        var timer;
-        
-        var h,m,s,c;
+      var timer,h, m, s, c;
+
+      // Hide-Show
+      $(".running,.abc,.stoped").hide();
+      $(".entertimeandhit").show();
+
+      // ----- Start -------------------------------------------
+
+      $(".start").click(function () {
         // Hide-Show
-        $(".running,.abc,.stoped").hide();
-        $(".entertimeandhit").show();
+        $(".abc,.stoped,.entertimeandhit,.restart,.resume").hide();
+        $(".start,.pause,.stop,.reset").show();
 
-        $(".start").click(function () {
-         
-          // Hide-Show
-          $(".abc,.stoped,.entertimeandhit").hide();
+        //Button Disabled
+        $(".start").attr("disabled", true);
 
-          //Button Disabled
-          $(".start").attr("disabled", true);
-          
-          $(".xyz").html(
-           "<h4>"+"Running"+"</h4>" 
-          );
+        $(".xyz").html(
+          "<h4>" + "Running" + "</h4>"
+        );
 
-          // Hide-Show
-          $(".start,.pause,.stop,.reset").show();
-          $(".restart,.resume").hide();
-      
-          var h = 0,m = 0,s = 0,c = 0;
-          
-          TimeClockSet(h,m,s,c);
-
-          timestart(h, m, s, c);
-
-        });
-
-        $(".resume").click(function () {
-          
-          // Hide-Show
-          $(".abc").show();
-          $(".stoped,.entertimeandhit").hide();
-          $(".start,.pause,.stop,.reset").show();
-          $(".restart,.resume").hide();
-          
-          //Button Disabled
-          $(".pause").attr("disabled", false);
-
-          $(".xyz").html(
-           "<h4>"+"Running"+"</h4>" 
-          );
-
-          h = $(".hour").text();
-          m = $(".minute").text();
-          s = $(".second").text();
-          c = $(".csecond").text();
-
-          timestart(h, m, s, c);
-        });
-
-        $(".restart").click(function () {
-         
-          // Hide-Show
-          $(".abc,.stoped,.entertimeandhit").hide();
-          $(".start,.pause,.stop,.reset,.xyz,.abcpause").show();
-          $(".resume,.restart").hide();
-
-          $(".xyz").html(
-           "<h4>"+"Running"+"</h4>" 
-          );
-
-         //Button Disabled
-         $(".stop,.pause").attr("disabled", false);
-         $(".start").attr("disabled", true);
-          
-         var h = 0,m = 0,s = 0,c = 0;
-
-         TimeClockSet(h,m,s,c)
-
-          timestart(h, m, s, c);
-        });
-
-        $(".pause").click(function () {
-
-          // Hide-Show
-          $(".running,.entertimeandhit,.stoped").hide();
-          $(".abc").show();
-          $(".resume,.pause,.stop,.reset").show();
-          $(".restart,.start").hide();
-
-          //Button Disabled
-          $(".pause").attr("disabled", true);
-         
-          $(".xyz").css("background-color","lightblue")
-        
-          h = $(".hour").text();
-          m = $(".minute").text();
-          s = $(".second").text();
-
-         
-          $(".xyz").html(
-           "<h4>"+"Pause: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>"+"</h4>" 
-          );
-
-          $(".abcpause").css("color","blue").append(
-            "<h6 class='hide' style='color:blue'>" + "Pause: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>"+ "<h6>" 
-          );
-        
-          clearInterval(timer);
-        });
-
-        $(".stop").click(function () {
-
-          // Hide-Show
-          $(".running,.entertimeandhit").hide();
-          $(".abc").show();
-          $(".restart,.pause,.stop,.reset").show();
-          $(".resume,.start").hide();
-
-          //Button Disabled
-          $(".stop,.pause").attr("disabled", true);
-          
-          $(".xyz").css("background-color","rgb(241, 194, 194)")
-
-          h = $(".hour").text();
-          m = $(".minute").text();
-          s = $(".second").text();
-        
-          $(".xyz").html(
-            "<h4>"+"Stop: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>"+ "<h4>" 
-          );
-
-          $(".abcpause").css("color","red").append(
-            "<h6 class='hide' style='color:red'>" + "Stop: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>"+ "<h6>" 
-          );
-
-          clearInterval(timer);
-        });
-
-        $(".reset").click(function () {
-
-          // Hide-Show
-          $(".running,.abc,.stoped").hide();
-          $(".entertimeandhit").show();
-          $(".start,.pause,.stop,.reset").show();
-          $(".resume,.restart").hide();
-          
-          //Button Disabled
-          $(".start,.stop,.pause").attr("disabled", false);
-
-          var h = 0,m = 0,s = 0,c = 0;
-
-          clearInterval(timer);
-          
-          TimeClockSet(h,m,s,c)
-
-          $(".xyz").empty();
-          $(".hide").text("").hide();
-          $("h6").hide();
-
-        });
-
-        function TimeClockSet(h,m,s,c){
-
-          // $(".hour").text((h < 10) ? ("0" + h) : h);
-          // $(".minute").text((m < 10) ? ("0" + m) : m);
-          // $(".second").text((s < 10) ? ("0" + s) : s);
-          // $(".csecond").text(c);    
-          
-          $(".hour").text(h);
-          $(".minute").text(m);
-          $(".second").text(s);
-          $(".csecond").text(c);    
-  
-        }
-      
-
-        function timestart(h, m, s, c) {
-          var h,m,s,c;
-          timer = setInterval(function () {
-            if (c == 0 || c >= 0) {
-              c++;
-              if (c == 100) {
-                c = 0;
-                s++;
-                if (s == 60) {
-                  s = 0;
-                  s++;
-                  m++;
-                  if (m == 60) {
-                    m = 0;
-                    m++;
-                    h++;
-                  }
-                }
-              }
-            }
-
-            TimeClockSet(h,m,s,c);
-          }, 10);
-        }
+        var h = 0,m = 0,s = 0,c = 0;
+        TimeClockSet(h, m, s, c);
+        timestart(h, m, s, c);
 
       });
 
- 
+      // ----- Resume -------------------------------------------
+
+      $(".resume").click(function () {
+
+        // Hide-Show
+        $(".abc").show();
+        $(".stoped,.entertimeandhit").hide();
+        $(".start,.pause,.stop,.reset").show();
+        $(".restart,.resume").hide();
+
+        //Button Disabled
+        $(".pause").attr("disabled", false);
+
+        $(".xyz").html(
+          "<h4>" + "Running" + "</h4>"
+        );
+
+        h = $(".hour").text();
+        m = $(".minute").text();
+        s = $(".second").text();
+        c = $(".csecond").text();
+
+        timestart(h, m, s, c);
+      });
+
+      // ----- Restart -------------------------------------------
+
+      $(".restart").click(function () {
+
+        // Hide-Show
+        $(".abc,.stoped,.entertimeandhit").hide();
+        $(".start,.pause,.stop,.reset,.xyz,.abcpause").show();
+        $(".resume,.restart").hide();
+
+        $(".xyz").html(
+          "<h4>" + "Running" + "</h4>"
+        );
+
+        //Button Disabled
+        $(".stop,.pause").attr("disabled", false);
+        $(".start").attr("disabled", true);
+
+        var h = 0,m = 0,s = 0,c = 0;
+        TimeClockSet(h, m, s, c)
+        timestart(h, m, s, c);
+      });
+
+      // ----- Pause -------------------------------------------
+
+      $(".pause").click(function () {
+
+        // Hide-Show
+        $(".running,.entertimeandhit,.stoped").hide();
+        $(".abc").show();
+        $(".resume,.pause,.stop,.reset").show();
+        $(".restart,.start").hide();
+
+        //Button Disabled
+        $(".pause").attr("disabled", true);
+
+        $(".xyz").css("background-color", "lightblue")
+
+        h = $(".hour").text();
+        m = $(".minute").text();
+        s = $(".second").text();
+
+
+        $(".xyz").html(
+          "<h4>" + "Pause: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>" + "</h4>"
+        );
+
+        $(".abcpause").css("color", "blue").append(
+          "<h6 class='hide' style='color:blue'>" + "Pause: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>" + "<h6>"
+        );
+
+        clearInterval(timer);
+      });
+
+      // ----- Stop -------------------------------------------
+
+      $(".stop").click(function () {
+
+        // Hide-Show
+        $(".running,.entertimeandhit").hide();
+        $(".abc").show();
+        $(".restart,.pause,.stop,.reset").show();
+        $(".resume,.start").hide();
+
+        //Button Disabled
+        $(".stop,.pause").attr("disabled", true);
+
+        $(".xyz").css("background-color", "rgb(241, 194, 194)")
+
+        h = $(".hour").text();
+        m = $(".minute").text();
+        s = $(".second").text();
+
+        $(".xyz").html(
+          "<h4>" + "Stop: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>" + "<h4>"
+        );
+
+        $(".abcpause").css("color", "red").append(
+          "<h6 class='hide' style='color:red'>" + "Stop: " + "<b>" + "hour:" + h + "min:" + m + "sec:" + s + "</b>" + "<h6>"
+        );
+
+        clearInterval(timer);
+      });
+
+       // ----- Reset -------------------------------------------
+
+      $(".reset").click(function () {
+        // Hide-Show
+        $(".running,.abc,.stoped").hide();
+        $(".entertimeandhit").show();
+        $(".start,.pause,.stop,.reset").show();
+        $(".resume,.restart").hide();
+
+        //Button Disabled
+        $(".start,.stop,.pause").attr("disabled", false);
+
+        var h = 0,
+          m = 0,
+          s = 0,
+          c = 0;
+
+        clearInterval(timer);
+
+        TimeClockSet(h, m, s, c)
+
+        $(".xyz").empty();
+        $(".hide").text("").hide();
+        $("h6").hide();
+
+      });
+
+      function TimeClockSet(h, m, s, c) {
+
+        $(".hour").text((parseInt(h) < 10) ? ("0" + parseInt(h)) : h);
+        $(".minute").text((parseInt(m) < 10) ? ("0" + parseInt(m)) : m);
+        $(".second").text((parseInt(s) < 10) ? ("0" + parseInt(s)) : s);
+        $(".csecond").text((parseInt(c) < 10) ? ("0" + parseInt(c)) : c);
+
+      }
+
+
+      function timestart(h, m, s, c) {
+        var h, m, s, c;
+        timer = setInterval(function () {
+          if (c == 0 || c >= 0) {
+            c++;
+            if (c == 100) {
+              c = 0;
+              s++;
+              if (s == 60) {
+                s = 0;
+                s++;
+                m++;
+                if (m == 60) {
+                  m = 0;
+                  m++;
+                  h++;
+                }
+              }
+            }
+          }
+
+          TimeClockSet(h, m, s, c);
+        }, 1);
+      }
+
+    });
---
+++ Task-4/js/jquery.js
@@ -0,0 +1,252 @@
+// script task 4 New
+
+$(document).ready(function () {
+  var index, indexbutton;
+  var x, length;
+  bool = false;
+
+  var no,ffname,llname,ggender,eemail,ccontact,ddob,ssports,aaboutyourself,tterms;
+
+  $(".content").hide().first().show();
+  $("button.previous, button.submit,.update,.cancel").hide();
+
+    //click on edit butten show hide
+
+    function buttonset() {
+        if (bool == true) {
+          if (length == x + 1) {
+            $("button.update, button.cancel").show();
+            $("button.next,button.submit").hide();
+          } else {
+            $("button.update, button.cancel").hide();
+            $("button.next").show();
+          }
+        }
+      }
+    
+    //form data get
+    function formdateget(){
+        no = $("tr").length;
+        ffname = $("#fname").val();
+        llname = $("#lname").val();
+        ggender = $("input[type='radio']:checked").val();
+        eemail = $("#email").val();
+        ccontact = $("#contact").val();
+        ddob = $("#dob").val();
+        ssports = $("select").children("option:selected").val();
+        aaboutyourself = $("#aboutyourself").val();
+        tterms = $("input[type='checkbox']:checked").val();
+
+        
+    }
+
+  //   -----------button nav------------
+
+  $(".button_nav").click(function () {
+
+    indexbutton = $(this).index();
+    $(".content").hide().eq($(this).index()).show();
+
+    //x == index of this button
+    x = $(this).index();
+    length = $(".content").length;
+    
+    if (x == 0) {
+      $(".previous,.submit,.update").hide();
+      $("button.next").show();
+    } else if (length == x + 1) {
+      $("button.next,.update").hide();
+      $("button.submit, button.previous").show();
+    } else {
+      $("button.submit").hide();
+      $("button.next, button.previous").show();
+    }
+
+    //function for button set click edit buuton click event
+    buttonset();
+  });
+
+//   -----------next button------------
+
+  $(".next").click(function () {
+    length = $(".content").length;
+    $(".content").hide();
+    if (x == null) {
+      x = 0;
+    }
+    x++;
+    if (x == length - 1) {
+      $("button.next").hide();
+      $("button.submit").show();
+    }
+
+    $("button.previous").show();
+    $(".content").eq(x).show();
+    $(".content").not($(".content").eq(x)).hide();
+
+     //function for button set click edit buuton click event
+    buttonset();
+  });
+
+  //   -----------previous button------------
+
+  $(".previous").click(function () {
+    $(".content").hide();
+    x--;
+    if (x == 0) {
+      $("button.previous").hide();
+    }
+    $(".content").eq(x).show();
+    $(".content").not($(".content").eq(x)).hide();
+    $("button.next").show();
+    $(".submit,.update").hide();
+    
+    //function for button set click edit buuton click event
+    buttonset();
+  });
+
+  //   -----------submit button------------
+
+  $(".submit").click(function () {
+    
+    formdateget();
+
+    if ($("#terms").is(":checked")) {
+        var tterms = "Accept";
+        } else {
+        var tterms = "Not Accept";
+        }
+
+    $(".row").append(`<tr class="removerow"><td class="no"> ${no} </td>,
+        <td class="efname"> ${ffname} </td>,
+        <td class="elname"> ${llname} </td>,
+        <td class="egender"> ${ggender} </td>,
+        <td class="eeemail"> ${eemail} </td>,
+        <td class="econtact"> ${ccontact} </td>,
+        <td class="edob"> ${ddob} </td>,
+        <td class="esports">${ssports}</td>,
+        <td class="eaboutyourself"> ${aaboutyourself} </td>,
+        <td class="eterms"> ${tterms} </td>,
+        <td> <button class="edit">Edit</button> </td>,
+        <td> <button class="deleterow">Delete</button> </td></tr>`);
+
+    //clear text box
+    $('.formreset')[0]. reset();
+  });
+
+
+  //   -----------edit button------------
+
+  $(document).on("click", ".edit", function () {
+    bool = true;
+    length = $(".content").length;
+
+    var eindex = $(".edit").index(this);
+    var index = $(".no").eq(eindex).text();
+    var efname = $(".efname").eq(eindex).text();
+    var elname = $(".elname").eq(eindex).text();
+    var egender = $(".egender").eq(eindex).text();
+    var eeemail = $(".eeemail").eq(eindex).text();
+    var econtact = $(".econtact").eq(eindex).text();
+    var edob = $(".edob").eq(eindex).text();
+    var esports = $(".esports").eq(eindex).text();
+    var eaboutyourself = $(".eaboutyourself").eq(eindex).text();
+    var eterms = $(".eterms").eq(eindex).text();
+
+    $(".index").val(index);
+
+    $("input.setedittextfname").val(efname);
+    $("input.setedittextlname").val(elname);
+    egender == " male "
+      ? $("#male").prop("checked", true)
+      : $("#female").prop("checked", true);
+    $("input.setedittextemail").val(eeemail);
+    $("input.setedittextcontact").val(econtact);
+    $("input.setedittextdob").val(edob);
+
+    if (esports == "cricket") {
+      $("option[value=cricket]").prop("selected", true);
+    } else if (esports == "football") {
+      $("option[value=football]").prop("selected", true);
+    } else {
+      $("option[value=chess]").prop("selected", true);
+    }
+
+    $("#aboutyourself").val(eaboutyourself);
+
+    if (eterms == " Accept ") {
+      $("#terms").prop("checked", true);
+      console.log("if");
+    } else {
+      $("#terms").prop("checked", false);
+    }
+
+    //function for button set click edit buuton click event
+    buttonset();
+  });
+
+  //   -----------update button------------
+
+    $(".update").click(function () {
+    $(".update,.cancel").hide();
+    $(".submit").show();
+    bool = false;
+    buttonset();
+
+    //form data get
+    formdateget();
+
+    if ($("#terms").is(":checked")) {
+      var tterms = "Accept";
+    } else {
+      var tterms = "Not Accept";
+    }
+
+    index = parseInt($(".index").val());
+
+    let html = `<td class="no"> ${index} </td>,
+         <td class="efname"> ${ffname} </td>,
+         <td class="elname"> ${llname} </td>,
+         <td class="egender"> ${ggender} </td>,
+         <td class="eeemail"> ${eemail} </td>,
+         <td class="econtact"> ${ccontact} </td>,
+         <td class="edob"> ${ddob} </td>,
+         <td class="esports"> ${ssports} </td>,
+         <td class="eaboutyourself"> ${aaboutyourself} </td>,
+         <td class="eterms"> ${tterms} </td>,
+         <td> <button class="edit">Edit</button> </td>,
+         <td> <button class="deleterow">Delete</button> </td>`;
+
+    $(".removerow")
+      .eq(parseInt(index - 1))
+      .html(html);
+
+    //clear text box
+    $('.formreset')[0]. reset();
+  });
+
+  //   -----------cancel button------------
+
+  $(document).on("click", ".cancel", function () {
+    bool = false;
+    buttonset();
+
+    //clear text box
+    $('.formreset')[0]. reset();
+
+    $(".update,.cancel").hide();
+    $(".submit").show();
+  });
+
+  //   -----------delete button------------
+
+  $(document).on("click", ".deleterow", function () {
+    var index = $(".deleterow").index(this);
+    $(".removerow").eq(index).remove();
+
+    $(".row tr").each(function (i) {
+      $($(this).find("td")[0]).html(i);
+    });
+  });
+
+});
