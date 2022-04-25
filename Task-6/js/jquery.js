834b65a515e57159b12f1ad925784c65e7989f81
jeel <koradiyajeel95@gmail.com>
2022-04-25 19:36:44 +0530

    task-6

---
+++ Task-6/js/jquery.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+    $("body").click(function(){
+        $(".form_exp").focus();
+    })
+
+    // All Clear
+    $("#allClear").click(function() {
+        $(".form_exp").val("");
+        $(".form_exp").focus();
+    });
+
+    // Backspace
+    $('#backspace').click(function() {
+        var valuebackspace = $(".form_exp").val();
+        if (!(parseInt(parseFloat(valuebackspace)) == 0 && valuebackspace.length == 1))
+            $(".form_exp").val(valuebackspace.slice(0, valuebackspace.length - 1));
+        if (valuebackspace.length == 1)
+            $(".form_exp").val("0");
+    });
+
+    //Adding to the expression
+    $(".clickbutton").click(function(){
+        var value = $(this).val();
+        var t = eval($(".form_exp").val($('.form_exp').val()+ value)); 
+    });
+
+    $(".equalto").click(function(){
+       
+        try {
+           var value = $('.form_exp').val();
+           console.log(value);
+           var value = value.replace(/x/g,"*");
+           var value = value.replace(/÷/g,"/");
+           var value = value.replace(/√/g,"**0.5");
+           var value = value.replace(/²/g,"**2");
+           
+           
+
+           var cal_result = (eval(value));
+           if(cal_result=="Infinity") throw "Division by zero is undefined";
+           $(".form_exp").val(cal_result);
+
+        } catch (e) {
+
+            if (e instanceof SyntaxError) {        
+                $(".form_exp").val("Malformed expression");
+            }
+            else{
+                $(".form_exp").val(e);
+            }
+          
+        }
+
+    });
+
+    // $(".squreroot").click(function(){
+    //     var t = $(".form_exp").val();
+    //     console.log(t);
+    //     var t = eval($(".form_exp").val());
+    //     var cal_result = Math.sqrt(t);
+    //     console.log(cal_result); 
+    //     $(".form_exp").val(cal_result);
+    // });
+
+    
+
+
+});
+
+$(document).on('keydown', function(event) {
+      if (event.key == "Escape") {
+        $(".form_exp").val("");
+      }
+
+    keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            cal_result = (eval(($(".form_exp").val())));
+            $(".form_exp").val(cal_result);
+        }
+
+
+});
