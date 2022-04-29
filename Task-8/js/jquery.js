375b88d165a0b661af7ef26ae7c1451c3ab621bf
jeel <koradiyajeel95@gmail.com>
2022-04-29 18:05:33 +0530

    task_8

--- Task-8/js/jquery.js
+++ Task-8/js/jquery.js
@@ -5,8 +5,6 @@ var addchildindex,addmorelength;
 var tabletitleindex,tablesubtitleindex,tablevalueindexx;
 $(document).ready(function(){
 
-  
-
     $(".showtable").append(`
     <table id="0" class="deleteleftrightside table-bordered" border="1">
       <thead>
@@ -19,6 +17,21 @@ $(document).ready(function(){
 
     //Add More Button with confirm box
     $(".addmore").click(function(){
+
+      bootbox.confirm({ 
+        message: "Are you sure add one more field?",
+        buttons: {
+          confirm: {
+              label: 'Yes',
+              className: 'btn-success'
+          },
+          cancel: {
+              label: 'No',
+              className: 'btn-danger'
+          }
+      },
+        callback: function(result){
+          if(result==true){
    
             var addmoreindex = $(".addmore").index(this);
 
@@ -32,8 +45,12 @@ $(document).ready(function(){
                 <thead> <tr></tr> </thead>
                 <tbody></tbody>
               </table>`);
-            });
+            }
+          }
+      })
+    });
 });
+
 
 //subtitle
 $(document).on("keyup",".subtitle",function(){
@@ -47,13 +64,19 @@ $(document).on("keyup",".subtitle",function(){
   for(i=0;i<trlength;i++){
     var subtitle = $(this).parents(".addmoreadd").find(".subtitle").eq(i).val();
     var value = $(this).parents(".addmoreadd").find(".value").eq(i).val();
-  
-    // console.log("subtitle: "+subtitle,value);
-    $(this).parents().find("table").eq(tablesubtitleindex).find("tbody").append(`
+
+    if ($('.subtitle').val()=="" && $('.value').val()=="") {
+      $(".tabledelete").eq(tablesubtitleindex).remove();
+    }
+  else{
+        $(this).parents().find("table").eq(tablesubtitleindex).find("tbody").append(`
         <tr class="tabledelete">
           <td>${subtitle}</td>
           <td>${value}</td>
         </tr>`);
+  }
+    // console.log("subtitle: "+subtitle,value);
+    
   }
  
 })
@@ -71,12 +94,16 @@ $(document).on("keyup",".value",function(){
     var value = $(this).parents(".addmoreadd").find(".value").eq(i).val();
     // console.log("subtitle: "+subtitle,value);
      
-    $(this).parents().find("table").eq(tablesubtitleindex).find("tbody").append(`
-        <tr class="tabledelete">
-          <td>${subtitle}</td>
-          <td>${value}</td>
-        </tr>`);
-      
+    if ($('.value').val()=="" && $('.subtitle').val()=="") {
+      $(".tabledelete").eq(tablesubtitleindex).remove();
+    }
+    else{
+          $(this).parents().find("table").eq(tablesubtitleindex).find("tbody").append(`
+          <tr class="tabledelete">
+            <td>${subtitle}</td>
+            <td>${value}</td>
+          </tr>`);
+    }
   }
 
 })
@@ -111,18 +138,36 @@ $(document).on("keyup",".tabletitle",function()
 $(document).on("click", ".addchild",function()
   {
           var addchildbtnindex = this;
+          bootbox.confirm({ 
+            message: "Are you sure add one more Childfield?",
+            buttons: {
+              confirm: {
+                  label: 'Yes',
+                  className: 'btn-success'
+              },
+              cancel: {
+                  label: 'No',
+                  className: 'btn-danger'
+              }
+          },
+            callback: function(result){
+      
+              if(result==true){
           addchildindex = $(".addchild").index(addchildbtnindex);
           $(".addmoreadd").eq(addchildindex).append(`<div class="addchildforaddingstyle">
           <input class="subtitle" type="text" placeholder="SubTitle">
           <input class="value" type="text" placeholder="Value">
           <button type="button" class="deletechild adddeletestyle">Delete</button>
           </div>`); 
+        }
+      }
+    });
 });
 
 $(document).on("mouseenter", ".addmoredelete",function(){
 
     addmorelength = $(this).parents(".addmoreaddboxmain").find(".addmoreadd").length;
-    console.log(addmorelength);
+    // console.log(addmorelength);
 
 });
      
@@ -131,16 +176,38 @@ $(document).on("click", ".addmoredelete",function(){
 
         trlength = $(this).parents(".addmoreadd").find(".addchildforaddingstyle").length;
         console.log(trlength);
+
         if(trlength>0){
+          bootbox.alert("Please Delete a child first of all")
           $('.addmoredelete').prop('disabled', false);
         }
         else if(trlength==0 && addmorelength>1){
           var addmoredeleteindex = this;
+          bootbox.confirm({ 
+            message: "Are you sure want to delete?",
+            buttons: {
+              confirm: {
+                  label: 'Yes',
+                  className: 'btn-success'
+              },
+              cancel: {
+                  label: 'No',
+                  className: 'btn-danger'
+              }
+          },
+            callback: function(result){
+      
+              if(result==true){
+          
           var addmoredelete = $(".addmoredelete").index(addmoredeleteindex);
           $(".deleteleftrightside").eq(addmoredelete).remove();
           $(".addmoreadd").eq(addmoredelete).remove();
         }
+      }
+    });
+        }
         else if(addmorelength==1){
+          // bootbox.alert("Last Add more so you can not delete this");
           $('.addmoredelete').prop('disabled', false);
           console.log("hello"+addmorelength);
         }
@@ -153,7 +220,34 @@ $(document).on("click", ".addmoredelete",function(){
 //Child Delete Button with confirm box
 $(document).on("click", ".deletechild",function(){
         var addmoredeleteindex = this;
-        var deletechildindex = $(".deletechild").index(addmoredeleteindex);
-        $(".addchildforaddingstyle").eq(deletechildindex).remove();
+        bootbox.confirm({ 
+          message: "Are you sure want to delete?",
+          buttons: {
+            confirm: {
+                label: 'Yes',
+                className: 'btn-success'
+            },
+            cancel: {
+                label: 'No',
+                className: 'btn-danger'
+            }
+        },
+          callback: function(result){
+            if(result==true){
+            var deletechildindex = $(".deletechild").index(addmoredeleteindex);
+            $(".addchildforaddingstyle").eq(deletechildindex).remove();
+
+            // if ($('.value').val()=="" && $('.subtitle').val()=="") {
+            //   $(".tabledelete").eq(deletechildindex).empty();
+            // }
+        
         $(".tabledelete").eq(deletechildindex).remove();
+
+        $(".value").trigger('keyup');
+        $(".subtitle").trigger('keyup');
+
+
+      }
+    }
+  });
 });
